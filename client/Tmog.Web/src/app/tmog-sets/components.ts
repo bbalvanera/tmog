693ffb9ea6d5166104f9440ae3aadadc6621eaef
bbalvanera <bernardo.balvanera@gmail.com>
2017-12-05 11:59:27 -0600

    refactor: remove old MVC project folder

--- client/Tmog.Web/src/app/tmog-sets/components.ts
+++
@@ -1,9 +0,0 @@
-﻿import { TMogSetsComponent } from './tmog-sets.component';
-import { TMogSetComponent } from './tmog-set/tmog-set.component';
-import { TMogSetAddComponent } from './tmog-set-add/tmog-set-add.component';
-
-export const components = [
-    TMogSetsComponent,
-    TMogSetComponent,
-    TMogSetAddComponent
-];
